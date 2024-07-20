466ece2a64345d57b3c02e0f6be8c7f63afcc62a
Jia Kwon <83686088+jia5232@users.noreply.github.com>
2024-07-20 09:44:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,29 @@ URL = "https://star-peanuts.tistory.com/rss"
 RSS_FEED = feedparser.parse(URL)
 MAX_POST = 5
 
-markdown_text = """
-## ✅ Latest Blog Post
-
-"""  # list of blog posts will be appended here
-
-for idx, feed in enumerate(RSS_FEED['entries']):
-    if idx > MAX_POST:
-        break
-    else:
+def fetch_latest_blog_posts():
+    markdown_text = ""
+    for idx, feed in enumerate(RSS_FEED['entries']):
+        if idx >= MAX_POST:
+            break
         feed_date = feed['published_parsed']
         markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
-        
-f = open("README.md", mode="w", encoding="utf-8")
-f.write(markdown_text)
-f.close()
+    return markdown_text
+
+def update_readme(latest_posts):
+    with open("README.md", "r", encoding="utf-8") as readme:
+        content = readme.read()
+
+    start_marker = "<!-- BLOG-POST-LIST:START -->"
+    end_marker = "<!-- BLOG-POST-LIST:END -->"
+    start_idx = content.find(start_marker) + len(start_marker)
+    end_idx = content.find(end_marker)
+
+    new_content = content[:start_idx] + "\n" + latest_posts + content[end_idx:]
+
+    with open("README.md", "w", encoding="utf-8") as readme:
+        readme.write(new_content)
+
+if __name__ == "__main__":
+    latest_posts = fetch_latest_blog_posts()
+    update_readme(latest_posts)
